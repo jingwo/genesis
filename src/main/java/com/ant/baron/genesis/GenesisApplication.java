e9f6484e3f117fca6e85bc4e80098540acd1a157
jingwo.jw <jingwo.jw@alibaba-inc.com>
2018-05-18 17:53:39 +0800

    second commit

---
+++ src/main/java/com/ant/baron/genesis/GenesisApplication.java
@@ -0,0 +1,12 @@
+package com.ant.baron.genesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesisApplication.class, args);
+	}
+}
